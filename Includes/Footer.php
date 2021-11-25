332a1d75d0ac0d9746e0e3cda4632c4c871d426f
Group4-WWW <group4-www@onionmail.org>
2021-11-25 22:01:37 +0600

    Initial commit

---
+++ Includes/Footer.php
@@ -0,0 +1,11 @@
+
+<div class="footer">
+		<p>Viktoryia Hrechka</p>
+		<p><a href = "contacts.html"> viktoryia.hrechka@knf.stud.vu.lt</a></p>
+		<p>Vilnius University, Kaunas Faculty, ISCSen0</p>
+		<p>2021-2022</p>
+	
+	</div>
+	
+</body>
+</html>
---
+++ Includes/Header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About us</title>
+	<meta name="description" content="Owl online school" />
+	<meta name="keywords" content="courses,knowledge, online, education" />
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+	<div class="main">
+		
+		<div class="header">
+			<div class="logo">
+				<div class="logo_text">
+					<h1><a href="index.html">Owl online school</a></h1>
+					<h2>We work for your future</h2>
+				</div>
+			</div>
+			
+			<div class="menubar">
+				
+				<ul class="menu">
+					<li><a href="index.html">About us</a></li>	
+					<li><a href="school.html">Professional courses</a></li>
+					<li><a href="show.html">Language courses</a></li>
+					<li><a href="information.html">Information</a></li>
+					<li><a href="contacts.html">Contacts</a></li>
+					<li><a href="Signup.html">Sign up</a></li>
+				</ul>
+
+			</div>
+
+		</div>
