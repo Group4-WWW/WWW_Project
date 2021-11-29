2486e6c73649a25a55e051c676ac64453bf4c786
Faisal-Kazeem <92097179+Faisal-Kazeem@users.noreply.github.com>
2021-11-29 16:18:03 +0000

    yes
    changed the out look of the success page
    still have to work on welcome
    change outlook on the signup page
    done with view all users
    fixed forgot password
    fixed update password
    fixed update pass

--- welcome.php
+++ welcome.php
@@ -1,12 +1,20 @@
 <style>
 .mycss_2{
 	color: green;  
+    padding: 10px;
+    margin-top: 5%;
+    text-align: center;
+    font-size: 20px;
 }
 </style>
 
 <style>
 .mycss_3{
 	color: red;  
+    padding: 10px;
+    margin-top: 5%;
+    text-align: center;
+    font-size: 20px;
 }
 </style>
 
@@ -16,8 +24,9 @@
         require_once "includes/header.php";
         require_once "db/db_config.php";
         if(isset($_SESSION['fullname'])){
-            echo "<h1 class = 'mycss_2'> You have been successfully registered ".$_SESSION['fullname'].", Please login using your credentials. Thank you for registering!";
+            echo "<h1 class = 'mycss_2'> You have been successfully registered as ".$_SESSION['fullname'].", Please login using your credentials. Thank you for registering!";
             session_destroy();?>
+            
             <div class="site_content">
                 <div class="sidebar_container">
                     <div class="sidebar">
@@ -30,16 +39,31 @@
                         <p><?php if($flag) echo "<h1 class = 'mycss_3'> Username or password incorrect! Please click Forgot Password"?></p>
                         <div class="lables_passreg_text">
                             <span><a href="#">Forgot your password?</a></span> | <span><a href="signup.php">Sign up</a></span>
-                    </div>
-
-                    <div class="sidebar">
-                        <h2>Updates</h2>
-                        <span>16.10.2021</span>
-                        <p>We launched a new course for Spanish </p>
-                        <a href="information.php">read more</a>
-                    </div>
+                    </div>   
                 </div>
+
+                <div class="sidebar">
+					<h2>Search</h2>
+					<form method="post" action="#" id="search_form" >
+						<input type="search" name="search_field" placeholder="your request" />
+						<input type="submit" class="btn" value="find" />
+					</form>
+				</div>
+
+
+
+
              </div>
+
+           
+
+             
+
+
+                
+
+             
+
         <?php }
         else if(isset($_POST['login'])){  
             $username = strip_tags($_POST['username']);
@@ -56,33 +80,17 @@
                 header("location: index.php");
             }  
         ?>
-         <div class="site_content">
-			
-			<div class="sidebar_container">
-
-            <div class="sidebar">
-                 <h2>Login</h2>
-                <form action="<?php echo htmlentities($_SERVER['PHP_SELF'])?>" method="POST">
-                
-                <input type="text"  placeholder="username" name="username" value="<?php if($_SERVER['REQUEST_METHOD']== 'POST') echo strip_tags($_POST['username']);?>"/>
-                <input type="password" name="password" placeholder="password" />
-                <input type="submit" class="btn" value="sign in" name="login"/>
-                <p><?php if($flag) echo "<h1 class = 'mycss_3'> Username or password incorrect! Please click Forgot Password"?></p>
-                <div class="lables_passreg_text">
-                    <span><a href="#">Forgot your password?</a></span> | <span><a href="signup.php">Sign up</a></span>
-             </div>
-
-				<div class="sidebar">
-					<h2>Updates</h2>
-					<span>16.10.2021</span>
-					<p>We launched a new course for Spanish </p>
-					<a href="#">read more</a>
-				</div>
-	
-		 </div>
+        
         <?php }
             else{
                 header("location: signup.php");
             }
-            include_once "includes/Footer.php";
+        ?>
+
+                
+
+        </div>
+
+        <?php 
+		    require_once "includes/footer.php";
         ?>
